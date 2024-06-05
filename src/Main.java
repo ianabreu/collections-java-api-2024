83b1db9663bf089014a11e2e4fdb4963f3838747
ianabreu <ianabreudev@gmail.com>
2024-06-04 22:43:26 -0300

    feat: Create class SumNumbers

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import list.Pesquisa.BookCatalog;
+import list.Pesquisa.SumNumbers;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BookCatalog saraiva = new BookCatalog();
-        saraiva.addBook("O Senhor dos Aneis - As Duas Torres", "Tolkien", 1937);
-        saraiva.addBook("A Arte da Guerra", "Sun Tzu", 1981);
-        saraiva.addBook("A Arte da Guerra", "Sun Tzu", 1979);
-        saraiva.addBook("O Senhor dos Aneis - 2", "Tolkien", 1940);
-        System.out.println(saraiva.findByAuthor("tolkien"));
-        System.out.println(saraiva.findByYearInterval(1903, 1912));
-        System.out.println(saraiva.findByTitle("a Arte da Guerra"));
-
-
+        SumNumbers n = new SumNumbers();
+        n.addNumber(3);
+        n.addNumber(5);
+        n.addNumber(10);
+        n.addNumber(-7);
+        System.out.println(n.displayNumbers());
+        n.addNumber(53);
+        System.out.println(n.calculateSum());
+        System.out.println(n.findBiggestNumber());
+        System.out.println(n.findSmallestNumber());
 
     }
 }
